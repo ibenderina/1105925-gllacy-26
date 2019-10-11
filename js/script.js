4d1a2b9e5fec516761c5008e506832ebd4c5fdfc
Irina Lukina <ibenderina@gmail.com>
2019-10-11 11:28:47 +0300

    Поправила много по мелочи

--- js/script.js
+++ js/script.js
@@ -1,22 +1,13 @@
-
 var call_back_wrapper = document.querySelector(".call-back--wrapper");
 call_back_wrapper.addEventListener("click", function (evt) {
-var el = evt.target;
-if (!(el && evt.target.closest("form"))) {
-call_back_wrapper.classList.remove("active");
-}
+  var el = evt.target;
+  if (!(el && evt.target.closest("form"))) {
+    call_back_wrapper.classList.remove("active");
+  }
 });
 
 var address__button = document.querySelector(".address__button");
-
 address__button.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  call_back_wrapper.classList.add("active");
-});
-
-var mapLink = document.querySelector(".map");
-
-mapLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   call_back_wrapper.classList.add("active");
 });
