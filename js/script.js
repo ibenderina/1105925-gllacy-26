4ee6dc31c4793089f665d45dc75188d0c724bd98
Irina Lukina <ibenderina@gmail.com>
2019-10-13 14:12:38 +0300

    Изменения по комментариям наставника

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 (function() {
-
   if (!Element.prototype.matches) {
     Element.prototype.matches = Element.prototype.matchesSelector ||
       Element.prototype.webkitMatchesSelector ||
       Element.prototype.mozMatchesSelector ||
       Element.prototype.msMatchesSelector;
   }
-
 })();
 
 (function() {
